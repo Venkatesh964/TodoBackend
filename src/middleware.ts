1681a5126d85e3d3862212c27fbabb89df1a7f52
Akhil <akhil@Venkatesh-MacBook-Air.local>
2024-02-29 21:31:35 +0530

    removed comments

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,14 +4,12 @@ import JWT_SECRET from "./config";
 function authMiddleware(req:any,res:any,next:any){
     try{
     const header=req.headers["authorization"];
-    //console.log(header);
     const token=header.split(" ")[1];
     if(!token){
         req.json({
             msg:"token is invalid"
         })
     }
-  
     const decode:any=jwt.verify(token,JWT_SECRET);
 
     req.body.id=decode.id;
