eae3f8cc91c4091639ce87ae528d16f6d917abb5
Akhil <akhil@Venkatesh-MacBook-Air.local>
2024-02-29 21:10:34 +0530

    removed comments

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express, { Router } from "express";
 
-// const router=express.Router;
 import cors from 'cors';
 import router from "./routes";
 
@@ -10,10 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/v1",router);
 
-// app.get("/",(req,res)=>{
-//     console.log("hi there");
-//     return;
-// })
 
 app.listen(3000,()=>{
     console.log("listening on port 3000");
